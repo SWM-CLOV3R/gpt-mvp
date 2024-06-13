62b6fed29cb138d6b5036431e8f149aeba0c57ab
R3D1NM <r3dinm@gmail.com>
2024-06-13 12:31:06 +0900

    [ui] main page & main Card (ONE-86)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import MainCard from './components/MainCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="bg-black flex justify-center">
+    <div className="App h-svh flex flex-col justify-between">
+      <header className="bg-black flex justify-center min-h-[5svh]">
         <h2 className='text-white text-3xl font-Bayon'> One!t </h2>
       </header>
+      <main className='max-h-[90svh] flex w-svw justify-center mb-3 mt-3'>
+        <MainCard />
+      </main>
+      <footer className='bg-slate-100 text-xs justify-evenly flex flex-col font-light items-center min-h-[5svh]'>
+        <p>Created by Team.CLOV3R</p>
+        <p>Powerd by chatGPT</p>
+      </footer>
     </div>
   );
 }
